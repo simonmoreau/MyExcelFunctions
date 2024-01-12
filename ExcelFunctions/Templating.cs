a61c782819fdfd98cfc0f3515a85a0435e6cc56f
Simon Moreau <simon@bim42.com>
2024-01-12 10:20:40 +0100

    Enable PhysicalFileProvider for included template

--- ExcelFunctions/Templating.cs
+++ ExcelFunctions/Templating.cs
@@ -2,6 +2,7 @@
 using ExcelDna.Integration;
 using ExcelFunctions.Services;
 using Fluid;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.VisualBasic;
 using System;
 using System.Collections;
@@ -26,7 +27,8 @@ namespace ExcelFunctions
         [ExcelFunction(Category = "Templating", Description = "Converts the provided value into a JSON string.", HelpTopic = "Converts the provided value into a JSON string.")]
         public static object RENDERTEMPLATE(
             [ExcelArgument("template", Name = "template", Description = "The template.")] string template,
-            [ExcelArgument("table", Name = "table", Description = "The table of values to render.")] object values)
+            [ExcelArgument("table", Name = "table", Description = "The table of values to render.")] object values,
+            [ExcelArgument("[includeFolder]", Name = "[includeFolder]", Description = "A folder containing linked template with {% include %}.")] object includeFolder)
         {
             if (values is object[,])
             {
@@ -37,16 +39,23 @@ namespace ExcelFunctions
                     Type objectType = null;
                     List<object> objects = ObjectsListBuilder.BuilObjectList(inputArray, out objectType);
 
-                    TemplateOptions.Default.MemberAccessStrategy = new UnsafeMemberAccessStrategy();
-                    FluidParser parser = new FluidParser();
+                    TemplateOptions templateOptions = TemplateOptions.Default;
+                    templateOptions.MemberAccessStrategy = new UnsafeMemberAccessStrategy();
 
+                    string includeFolderValue = Optional.Check(includeFolder, "");
+                    if (!string.IsNullOrEmpty(includeFolderValue))
+                    {
+                        templateOptions.FileProvider = new PhysicalFileProvider(includeFolderValue);
+                    }
+
+                    FluidParser parser = new FluidParser();
 
                     object model = new { pages = objects };
                     string source = template;
 
                     if (parser.TryParse(source, out IFluidTemplate? fluidTemplate, out string? error))
                     {
-                        TemplateContext context = new TemplateContext(model);
+                        TemplateContext context = new TemplateContext(model, templateOptions);
 
                         string renderedValue = fluidTemplate.Render(context);
 
@@ -71,9 +80,10 @@ namespace ExcelFunctions
 
         [ExcelFunction(Category = "Templating", Description = "Converts the provided value into a JSON string.", HelpTopic = "Converts the provided value into a JSON string.")]
         public static object RENDERTEMPLATETOFILE(
-    [ExcelArgument("path", Name = "path", Description = "The file to write to.")] string path,
-    [ExcelArgument("template", Name = "template", Description = "The template.")] string template,
-    [ExcelArgument("table", Name = "table", Description = "The table of values to render.")] object values)
+[ExcelArgument("path", Name = "path", Description = "The file to write to.")] string path,
+[ExcelArgument("template", Name = "template", Description = "The template.")] string template,
+[ExcelArgument("table", Name = "table", Description = "The table of values to render.")] object values,
+[ExcelArgument("[includeFolder]", Name = "[includeFolder]", Description = "A folder containing linked template with {% include %}.")] object includeFolder)
         {
             if (values is object[,])
             {
@@ -84,7 +94,16 @@ namespace ExcelFunctions
                     Type objectType = null;
                     List<object> objects = ObjectsListBuilder.BuilObjectList(inputArray, out objectType);
 
-                    TemplateOptions.Default.MemberAccessStrategy = new UnsafeMemberAccessStrategy();
+
+                    TemplateOptions templateOptions = TemplateOptions.Default;
+                    templateOptions.MemberAccessStrategy = new UnsafeMemberAccessStrategy();
+
+                    string includeFolderValue = Optional.Check(includeFolder, "");
+                    if (!string.IsNullOrEmpty(includeFolderValue))
+                    {
+                        templateOptions.FileProvider = new PhysicalFileProvider(includeFolderValue);
+                    }
+
                     FluidParser parser = new FluidParser();
 
 
@@ -93,7 +112,7 @@ namespace ExcelFunctions
 
                     if (parser.TryParse(source, out IFluidTemplate? fluidTemplate, out string? error))
                     {
-                        TemplateContext context = new TemplateContext(model);
+                        TemplateContext context = new TemplateContext(model, templateOptions);
 
                         string renderedValue = fluidTemplate.Render(context);
 
