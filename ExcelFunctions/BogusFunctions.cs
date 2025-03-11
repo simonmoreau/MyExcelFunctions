ce70e4cd6e8ca74490271c7b5e046711beb20835
Simon Moreau <simon@bim42.com>
2025-03-11 12:58:01 +0100

    Bugos function to generate fake data

---
+++ ExcelFunctions/BogusFunctions.cs
@@ -0,0 +1,154 @@
+﻿using Bogus;
+using ExcelDna.Integration;
+using ExcelFunctions.Services;
+using FuzzySharp;
+using Markdig;
+using Microsoft.Extensions.FileSystemGlobbing.Internal;
+using System;
+using System.Data.Common;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using static Bogus.DataSets.Name;
+
+namespace ExcelFunctions
+{
+    public static class BogusFunctions
+    {
+        // Parameters:
+        //   gender:
+        //     For locale's that support Gender naming.
+
+        [ExcelFunction(Category = "String", Description = "Get a first name.", HelpTopic = "Get a first name.")]
+        public static object BOGUSFIRSTNAME(
+    [ExcelArgument("[gender]", Name = "[gender]", Description = "(Optional) For locale's that support Gender naming.")] object gender,
+    [ExcelArgument("[locale]", Name = "[locale]", Description = "(Optional) The current locale for the dataset.")] object locale,
+    [ExcelArgument("[seed]", Name = "[seed]", Description = "(Optional) A number used to calculate a starting value for the pseudo-random number sequence.")] object seed)
+        {
+            Faker faker = CreateFaker(locale, seed);
+
+            return BogusGenerateWithGender(gender, locale, seed, faker, faker.Name.FirstName);
+        }
+
+        [ExcelFunction(Category = "String", Description = "Get a last name.", HelpTopic = "Get a last name.")]
+        public static object BOGUSLASTNAME(
+[ExcelArgument("[gender]", Name = "[gender]", Description = "(Optional) For locale's that support Gender naming.")] object gender,
+[ExcelArgument("[locale]", Name = "[locale]", Description = "(Optional) The current locale for the dataset.")] object locale,
+[ExcelArgument("[seed]", Name = "[seed]", Description = "(Optional) A number used to calculate a starting value for the pseudo-random number sequence.")] object seed)
+        {
+            Faker faker = CreateFaker(locale, seed);
+
+            return BogusGenerateWithGender(gender, locale, seed, faker, faker.Name.LastName);
+        }
+
+        [ExcelFunction(Category = "String", Description = "Gets a random job title.", HelpTopic = "Gets a random job title.")]
+        public static object BOGUSJOBTITLE(
+[ExcelArgument("[locale]", Name = "[locale]", Description = "(Optional) The current locale for the dataset.")] object locale,
+[ExcelArgument("[seed]", Name = "[seed]", Description = "(Optional) A number used to calculate a starting value for the pseudo-random number sequence.")] object seed)
+        {
+            Faker faker = CreateFaker(locale, seed);
+
+            return faker.Name.JobTitle();
+        }
+
+        [ExcelFunction(Category = "String", Description = "Get a type of job.", HelpTopic = "Get a type of job.")]
+        public static object BOGUSJOBTYPE(
+[ExcelArgument("[locale]", Name = "[locale]", Description = "(Optional) The current locale for the dataset.")] object locale,
+[ExcelArgument("[seed]", Name = "[seed]", Description = "(Optional) A number used to calculate a starting value for the pseudo-random number sequence.")] object seed)
+        {
+            Faker faker = CreateFaker(locale, seed);
+
+            return faker.Name.JobType();
+        }
+
+        [ExcelFunction(Category = "String", Description = "Generates a legit Internet URL avatar from twitter accounts.", HelpTopic = "Generates a legit Internet URL avatar from twitter accounts.")]
+        public static object BOGUSAVATAR(
+[ExcelArgument("[locale]", Name = "[locale]", Description = "(Optional) The current locale for the dataset.")] object locale,
+[ExcelArgument("[seed]", Name = "[seed]", Description = "(Optional) A number used to calculate a starting value for the pseudo-random number sequence.")] object seed)
+        {
+            Faker faker = CreateFaker(locale, seed);
+
+            return faker.Internet.Avatar();
+        }
+
+        [ExcelFunction(Category = "String", Description = "Helper method to pick a random element.", HelpTopic = "Helper method to pick a random element.")]
+        public static object BOGUSPICKRANDOM(
+[ExcelArgument("values", Name = "values", Description = "A list of elements to pick from.")] object values,
+[ExcelArgument("[locale]", Name = "[locale]", Description = "(Optional) The current locale for the dataset.")] object locale,
+[ExcelArgument("[seed]", Name = "[seed]", Description = "(Optional) A number used to calculate a starting value for the pseudo-random number sequence.")] object seed)
+        {
+            Faker faker = CreateFaker(locale, seed);
+
+            object[,] inputArray = (object[,])values;
+
+            List<string> list = FlattenArray(inputArray).Distinct().ToList();
+
+            return faker.PickRandom(list);
+        }
+
+        private static object BogusGenerateWithGender(object gender, object locale, object seed, Faker faker, Func<Gender?,string> func)
+        {
+            try
+            {
+                string name = func(faker.PickRandom<Gender>());
+
+                string? genderTyped = Optional.Check(gender, null);
+                if (genderTyped != null)
+                {
+                    if (genderTyped.ToLower() == "male")
+                    {
+                        name = func(Gender.Male);
+                    }
+                    else if (genderTyped.ToLower() == "female")
+                    {
+                        name = func(Gender.Female);
+                    }
+                }
+
+                return name;
+            }
+            catch
+            {
+                return ExcelDna.Integration.ExcelError.ExcelErrorNA;
+            }
+        }
+
+        private static Faker CreateFaker(object locale, object seed)
+        {
+            int typedSeed = Optional.Check(seed, -1);
+            if (typedSeed != -1)
+            {
+                Randomizer.Seed = new Random(typedSeed);
+            }
+
+            Faker faker = new Faker("fr");
+            string? typedLocatle = Optional.Check(locale, null);
+            if (typedLocatle != null)
+            {
+                faker = new Faker(typedLocatle);
+            }
+
+            return faker;
+        }
+
+        public static List<string?> FlattenArray(object[,] inputArray)
+        {
+            List<string?> flattenedList = new List<string?>();
+
+            int rows = inputArray.GetLength(0);
+            int columns = inputArray.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    string? value = Optional.Check(inputArray[i, j], null);
+                    flattenedList.Add(value);
+                }
+            }
+
+            return flattenedList;
+        }
+    }
+}
--- ExcelFunctions/Services/Optional.cs
+++ ExcelFunctions/Services/Optional.cs
@@ -9,7 +9,7 @@ namespace ExcelFunctions.Services
 {
     internal static class Optional
     {
-        internal static string Check(object arg, string defaultValue)
+        internal static string? Check(object arg, string? defaultValue)
         {
             if (arg is string)
                 return (string)arg;
@@ -17,27 +17,6 @@ namespace ExcelFunctions.Services
                 return defaultValue;
             else
                 return arg.ToString();  // Or whatever you want to do here....
-
-            // Perhaps check for other types and do whatever you think is right ....
-            //else if (arg is double)
-            //    return "Double: " + (double)arg;
-            //else if (arg is bool)
-            //    return "Boolean: " + (bool)arg;
-            //else if (arg is ExcelError)
-            //    return "ExcelError: " + arg.ToString();
-            //else if (arg is object[,](,))
-            //    // The object array returned here may contain a mixture of types,
-            //    // reflecting the different cell contents.
-            //    return string.Format("Array[{0},{1}]({0},{1})",
-            //      ((object[,](,)(,))arg).GetLength(0), ((object[,](,)(,))arg).GetLength(1));
-            //else if (arg is ExcelEmpty)
-            //    return "<<Empty>>"; // Would have been null
-            //else if (arg is ExcelReference)
-            //  // Calling xlfRefText here requires IsMacroType=true for this function.
-            //                return "Reference: " +
-            //                     XlCall.Excel(XlCall.xlfReftext, arg, true);
-            //            else
-            //                return "!? Unheard Of ?!";
         }
 
         internal static double Check(object arg, double defaultValue)
@@ -53,7 +32,7 @@ namespace ExcelFunctions.Services
 
         internal static int Check(object arg, int defaultValue)
         {
-            if (arg is int)
+            if (arg is int || arg is double)
                 return Convert.ToInt16(arg);
             else if (arg.GetType().Name == typeof(ExcelMissing).Name )
                 return defaultValue;
